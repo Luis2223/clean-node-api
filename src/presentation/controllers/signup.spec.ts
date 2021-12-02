047774715a4edeb9bfbfc7a087ff6bf1e0275d77
Luis Felipe <luis.santos@pontaltech.com.br>
2021-12-02 11:00:36 -0300

    test: ensure SignUpController returns 400 if no password confirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -44,18 +44,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
-  // test('Should return 400 if no email is provided', () => {
-  //   // system ungry test
-  //   const sut = new SignUpController()
-  //   const httpRequest = {
-  //     body: {
-  //       name: 'any_name',
-  //       password: 'any_password',
-  //       passwordConfirmation: 'any_password'
-  //     }
-  //   }
-  //   const httpResponse = sut.handle(httpRequest)
-  //   expect(httpResponse.statusCode).toBe(400)
-  //   expect(httpResponse.body).toEqual(new MissingParamError('email'))
-  // })
+  test('Should return 400 if no password confirmation is provided', () => {
+    // system ungry test
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
 })
