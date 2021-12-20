3a5ae69fb6798eceb7a7ea83f63377140318e5f0
Luis Felipe <luis.santos@pontaltech.com.br>
2021-12-20 11:28:01 -0300

    refactor: move SignUp protocols single file

---
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -0,0 +1,6 @@
+export * from '../../protocols'
+export * from '../../protocols/email-validator'
+export * from '../../helpers/http-helper'
+export * from '../../../domain/usecase/add-account'
+export * from '../../../domain/models/account'
+export * from '../../errors'
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,7 +1,14 @@
-import { AddAccount } from '../../domain/usecase/add-account'
-import { InvalidParamError, MissingParamError } from '../errors'
-import { badRequest, serverError } from '../helpers/http-helper'
-import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols'
+import {
+  Controller,
+  EmailValidator,
+  HttpRequest,
+  HttpResponse,
+  badRequest,
+  serverError,
+  InvalidParamError,
+  MissingParamError,
+  AddAccount
+} from './signup-protocols'
 
 export class SignUpController implements Controller {
   constructor (
